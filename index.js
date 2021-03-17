d5a5ec0bf2c481ccd8adedd929ab33f5428d33d5
Vitória Amorim <vitoria.amorimm01@gmail.com>
2021-03-16 23:59:53 -0400

    Create Js file and do all functions

---
+++ index.js
@@ -0,0 +1,20 @@
+function insert(num) {
+  let currentNum =  document.getElementById('result').innerHTML;
+  document.getElementById('result').innerHTML=currentNum+num;
+}
+
+function cleanCalc() {
+    document.getElementById('result').innerHTML=" ";
+
+}
+
+function calc() {
+    let finalResult=  document.getElementById('result').innerHTML;
+        if (finalResult) {
+            document.getElementById('result').innerHTML = eval(finalResult);
+        }
+        else {
+            document.getElementById('result').innerHTML = "NaN"
+        }
+
+}
